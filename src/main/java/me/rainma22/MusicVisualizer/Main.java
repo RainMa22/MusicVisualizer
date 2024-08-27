18e439762a30285c1507dbcf850bf38f63cca7c9
Rainma22 <d2rain@126.com>
2024-08-26 18:43:34 -0700

    set CHUNK_SIZE as static variable

--- src/main/java/me/rainma22/MusicVisualizer/Main.java
+++ src/main/java/me/rainma22/MusicVisualizer/Main.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
+    private static final int CHUNK_SIZE = 512;
+
     public static void main(String[] args) {
 
         if (args.length < 2){
@@ -21,16 +23,15 @@ public class Main {
                 me = new MusicExtractor(args[1]);
                 double[] frames = me.readFile();
                 System.out.printf("number of Channel: %d\n", me.getNumChannels());
-                int chunkSize= 512;
-                for (int j = 0; j < frames.length; j+=chunkSize) {
-                    double[] RE = Arrays.copyOfRange(frames,j,j+chunkSize);
+                for (int j = 0; j < frames.length; j+=CHUNK_SIZE) {
+                    double[] RE = Arrays.copyOfRange(frames,j,j+CHUNK_SIZE);
 
                     double[] IM = new double[RE.length];
                     double[] outRE = new double[RE.length];
                     double[] outIM = new double[RE.length];
                     FastFouriers.ITERATIVE_COOLEY_TUKEY.transform(RE, IM,outRE,outIM);
 
-                    for (int i = 0; i < chunkSize; i++) {
+                    for (int i = 0; i < CHUNK_SIZE; i++) {
                         System.out.printf("sample %d:channel %d: %f, converted to %f\n", (j+i)/2, i%2, RE[i], outRE[i]);
                     }
                 }
