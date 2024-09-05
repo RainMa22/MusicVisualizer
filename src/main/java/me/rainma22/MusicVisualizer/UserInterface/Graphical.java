bf1bd7bf2fba4a334866afa036d0198c8a066bcf
Rainma22 <d2rain@126.com>
2024-09-05 10:46:18 -0700

    fixed a bug where Graphical.java can not find GUI.fxml

--- src/main/java/me/rainma22/MusicVisualizer/UserInterface/Graphical.java
+++ src/main/java/me/rainma22/MusicVisualizer/UserInterface/Graphical.java
@@ -3,11 +3,17 @@ package me.rainma22.MusicVisualizer.UserInterface;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Graphical extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(""));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("GUI.fxml")));
+
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
