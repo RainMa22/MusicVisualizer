3aa96eb0a5827b34d3b07025939b93c4ab80f6a7
Rainma22 <d2rain@126.com>
2024-09-07 12:22:49 -0700

    added basic FileChooser Action

--- src/main/java/me/rainma22/musicvisualizer/ui/gui/FileBar.java
+++ src/main/java/me/rainma22/musicvisualizer/ui/gui/FileBar.java
@@ -1,30 +1,44 @@
 package me.rainma22.musicvisualizer.ui.gui;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FileBar extends JPanel {
     private JFileChooser chooser;
+    private JPanel leftPadding, rightPadding;
     private JTextField pathField;
     private JButton chooserBtn;
     private JLabel label;
 
     public FileBar(String name, String path) {
-//        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        setLayout(new BorderLayout());
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
         chooser = new JFileChooser(path);
         pathField = new JTextField(path);
         chooserBtn = new JButton("File");
-        label = new JLabel(name);
+        label = new JLabel(name, SwingConstants.CENTER);
+//        label.setBorder(new LineBorder(Color.BLACK, 1));
+        leftPadding = new JPanel();
+        rightPadding = new JPanel();
+        chooserBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = chooser.showOpenDialog(null);
+                if (result == JFileChooser.APPROVE_OPTION)
+                    pathField.setText(chooser.getSelectedFile().getAbsolutePath());
+            }
+        });
+        add(leftPadding);
         add(label);
         add(pathField);
         add(chooserBtn);
+        add(rightPadding);
     }
 
-    @Override
-    public void repaint() {
-        super.repaint();
+    public void addFileFilter(FileFilter filter){
+        chooser.addChoosableFileFilter(filter);
     }
 
     @Override
@@ -36,10 +50,12 @@ public class FileBar extends JPanel {
         setPreferredSize(selfDimensions);
         int childWidthUnit = selfDimensions.width / 10;
         int childHeightUnit = selfDimensions.height / 10;
-        setBorder(new EmptyBorder(childHeightUnit, childWidthUnit / 2, childHeightUnit, childWidthUnit / 2));
-        label.setPreferredSize(new Dimension(2 * childWidthUnit, 8 * childHeightUnit));
-        pathField.setPreferredSize(new Dimension(5 * childWidthUnit, 8 * childHeightUnit));
-        chooserBtn.setPreferredSize(new Dimension(2 * childWidthUnit, 8 * childHeightUnit));
-
+        leftPadding.setPreferredSize(new Dimension(childWidthUnit/2, 10*childWidthUnit));
+        label.setPreferredSize(new Dimension(childWidthUnit, 8 * childHeightUnit));
+        pathField.setPreferredSize(new Dimension(7* childWidthUnit, 8 * childHeightUnit));
+        chooserBtn.setPreferredSize(new Dimension(childWidthUnit, 8 * childHeightUnit));
+        rightPadding.setPreferredSize(new Dimension(childWidthUnit/2, 10*childWidthUnit));
     }
+
+
 }
--- src/main/java/me/rainma22/musicvisualizer/ui/gui/Graphical.java
+++ src/main/java/me/rainma22/musicvisualizer/ui/gui/Graphical.java
@@ -4,20 +4,31 @@ package me.rainma22.musicvisualizer.ui.gui;
 import javax.swing.*;
 import java.awt.*;
 public class Graphical extends JFrame {
+    private FileBar inputFileBar, outputFileBar;
+
     public Graphical(int width, int height){
         super("Music Visualizer");
+        JComponent dummy = new JTextField("dummy");
+        JPanel dummy2 = new JPanel();
+        inputFileBar = new FileBar("Input","");
+        outputFileBar = new FileBar("Output", "");
+
         setSize(new Dimension(width,height));
-        setResizable(false);
+        setPreferredSize(new Dimension(width,height));
+        setMinimumSize(new Dimension(640,360));
+        setResizable(true);
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-        add(new JTextField("Hello world!"));
-        add(new FileBar("test",""));
-        add(new FileBar("test2", ""));
+        dummy.setPreferredSize(new Dimension(width,height/10*7));
+        dummy2.setPreferredSize(new Dimension(width,height/10));
+        add(dummy);
+        add(inputFileBar);
+        add(outputFileBar);
+        add(dummy2);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     public void start(){
         setVisible(true);
         revalidate();
-
     }
 
 }
