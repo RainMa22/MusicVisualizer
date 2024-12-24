719ff8261990db5aa9170634d052fea02fdb2585
rain <rain@Rain-HPMini>
2024-12-23 19:39:46 -0800

    Added an abstraction layer for color and Image

---
+++ src/main/java/me/rainma22/musicvisualizer/Image/ColorProvider.java
@@ -0,0 +1,40 @@
+
+package me.rainma22.musicvisualizer.Image;
+
+import me.rainma22.musicvisualizer.Image.Resources.Image;
+import me.rainma22.musicvisualizer.Image.Resources.ResourceManager;
+
+public class ColorProvider {
+    private ColorRGBA color;
+    private String imageId;
+    
+    private ColorProvider(){
+        color = null;
+        imageId = null;
+    };
+    
+    public static ColorProvider ofColor(ColorRGBA color){
+        ColorProvider result = new ColorProvider();
+        result.color = color;
+        return result;
+    }
+    
+    public static ColorProvider ofImage(String imageId){
+        ColorProvider result = new ColorProvider();
+        result.imageId = imageId;
+        return result;
+    }
+    
+    public ColorRGBA getColor() throws UnsupportedOperationException{
+        if(color == null) throw new UnsupportedOperationException();
+        return color;
+    }
+    
+    public Image getImage(ResourceManager ResMan) 
+            throws UnsupportedOperationException{
+        if(imageId == null) throw new UnsupportedOperationException();
+        return ResMan.getImage(imageId);
+    }
+}
+
+
